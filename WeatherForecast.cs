c30ca655974df4a8c5e07367976d130d8db51f40
Pekeitu <106318905+Pekeitu@users.noreply.github.com>
2023-03-10 12:02:23 -0300

    Add files via upload

---
+++ WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace TP1Dai;
+
+public class Pizza
+{
+    public int id{ get; set; }
+
+    public string nombre{ get; set; }
+
+    public float precio{ get; set; } 
+
+    public bool libreDeGluten{ get; set; } 
+
+    public string descripcion{ get; set; }
+}
